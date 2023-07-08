c8d939917c604f0e598a50153cfcfa4d5d86bae7
cbot918 <cbot918@gmail.com>
2023-07-09 01:33:25 +0800

    impl sshy

---
+++ go.mod
@@ -0,0 +1,15 @@
+module ssh
+
+go 1.19
+
+require golang.org/x/crypto v0.11.0 // indirect
+
+require (
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/helloyi/go-sshclient v1.2.0
+	github.com/joho/godotenv v1.5.1
+	github.com/kr/fs v0.1.0 // indirect
+	github.com/pkg/sftp v1.13.5 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	ssh "github.com/helloyi/go-sshclient"
+	"github.com/joho/godotenv"
+)
+
+var (
+	lg = fmt.Println
+	lf = fmt.Printf
+)
+
+type Config struct {
+	Url      string
+	User     string
+	Password string
+	Key      string
+}
+
+func main() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	cfg := &Config{
+		Url:      os.Getenv("URL"),
+		User:     os.Getenv("USER"),
+		Password: os.Getenv("PASSWORD"),
+	}
+
+	// client, err := ssh.DialWithKey("127.0.0.1:2222", "vagrant", "./private_key")
+
+	sshy, err := NewSSHy(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	banner, err := sshy.getBanner()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		lf(banner)
+		// Read input from the user
+		scanner.Scan()
+		input := scanner.Text()
+
+		if input == "exit" {
+			break
+		}
+		result, err := sshy.ExecOne(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+		lg(result)
+
+	}
+
+}
+
+type SSHy struct {
+	Client *ssh.Client
+}
+
+func NewSSHy(cfg *Config) (*SSHy, error) {
+	ssh, err := ssh.DialWithPasswd(cfg.Url, cfg.User, cfg.Password)
+	if err != nil {
+		return nil, err
+	}
+	return &SSHy{
+		Client: ssh,
+	}, nil
+}
+
+func (s *SSHy) ExecOne(cmd string) (string, error) {
+	out, err := s.Client.Cmd(cmd).Output()
+	if err != nil {
+		return "", err
+	}
+	return string(out), nil
+}
+
+func (s *SSHy) getBanner() (string, error) {
+	whoami, err := s.ExecOne("whoami")
+	if err != nil {
+		return "", err
+	}
+	hostname, err := s.ExecOne("hostname")
+	if err != nil {
+		return "", err
+	}
+	pwd, err := s.ExecOne("pwd")
+	if err != nil {
+		return "", err
+	}
+	banner := fmt.Sprintf("%s@%s:%s$ ",
+		strings.TrimSuffix(whoami, "\n"),
+		strings.TrimSuffix(hostname, "\n"),
+		strings.TrimSuffix(pwd, "\n"))
+	return banner, nil
+}
